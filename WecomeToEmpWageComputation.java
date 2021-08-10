ccebcd4039e94b6f342df4044a4919456d1c4e6f
Ravi <ravi9519138461@gmail.com>
2021-08-10 21:19:52 +0530

    Added: Employee Check for Present or Absent

--- WecomeToEmpWageComputation.java
+++ WecomeToEmpWageComputation.java
@@ -1,6 +1,21 @@
 public class WecomeToEmpWageComputation {
-	public static void main (String [] ags) {
+	public static void main(String[] ags) {
 
 		System.out.println("WecomeToEmpWageComputation");
+		WecomeToEmpWageComputation m1 = new WecomeToEmpWageComputation();
+		m1.emp();
+	}
+
+	private void emp() {
+
+		int IS_Full_Time = 1;
+
+		double empcheck = (int) (Math.floor(Math.random() * 10) % 2);
+		if (empcheck == IS_Full_Time) {
+			System.out.println("Employee is present: " + empcheck);
+		} else {
+			System.out.println("Employee is absent: " + empcheck);
+		}
 	}
 }
+
